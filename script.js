f4f16e181c0e7a0bd0396aa5c86fd8f4149ccf4e
Davide Sibilano <157793240+zDado@users.noreply.github.com>
2024-09-23 11:28:01 +0200

    1

---
+++ script.js
@@ -0,0 +1,23 @@
+//Qui scriviamo il codice JS
+
+
+//let è una parolea chiave che serve a dichiarare delle varibili in js
+
+
+
+let myVariable;
+
+let firstName = prompt("Qual è il tuo nome?")
+
+//promt è una funzione che chiede un input all'utente e fun
+
+//mostra un pop up sulla finestra browser
+
+
+if(firstName == null){
+    
+}
+
+//console.Log() logga in console il messaggio passato come
+console.log("Benvenuto"+firstName)
+
