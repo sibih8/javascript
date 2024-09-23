3e659a411d2967f5f9fde2d62fc470f5a14223a9
Davide Sibilano <157793240+zDado@users.noreply.github.com>
2024-09-23 11:54:41 +0200

    1
    update

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-//Qui scriviamo il codice JS
+let dom =prompt("Quanti capelli hai in testa?")
+ans1 <ans1 class>"tolowercase"</ans1>
+if("")
+
+ans1 = ans1.trim()
 
 
-//let è una parolea chiave che serve a dichiarare delle varibili in js
 
-
-
-let myVariable;
-
-let firstName = prompt("Qual è il tuo nome?")
-
-//promt è una funzione che chiede un input all'utente e fun
-
-//mostra un pop up sulla finestra browser
-
-
-if(firstName == null){
-    
-}
-
-//console.Log() logga in console il messaggio passato come
-console.log("Benvenuto"+firstName)
 
